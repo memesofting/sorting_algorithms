a92b6518975fd7a0ebc82f30a9bb65ba82433c25
memesofting <bibitayoadekunle00@gmail.com>
2024-04-30 17:56:18 +0100

    on insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,1 +1,28 @@
 #include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascening order
+ * @list: doubly linked list to be sorted
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *temp;
+
+    if (*list == NULL || *list->next == NULL)
+    {
+        return;
+    }
+    while (*list->next != NULL)
+    {
+        if (*list->next->n < *list->prev->n)
+        {
+            temp = *list->next;
+            list->next = list->prev;
+            list->prev = temp
+        }
+        temp = list->next->next;
+        list->next = temp;
+    }
+}

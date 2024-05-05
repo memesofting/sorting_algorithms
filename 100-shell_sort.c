fd2c24dcc76142c226cc8cfd75beb1241da38493
memesofting <bibitayoadekunle00@gmail.com>
2024-05-05 17:35:00 +0100

    trying shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array of integers in ascending order
+ * using the shell sort algorithm
+ * using the Knuth sequence
+ */
+
+void shell_sort(int *array, size_t size)
+{
+    
+}
--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ void swap(int *a, int *b);
 int divide(int *array, int low, int high, size_t size);
 void quick_sort_recursion(int *array, int low, int high, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
